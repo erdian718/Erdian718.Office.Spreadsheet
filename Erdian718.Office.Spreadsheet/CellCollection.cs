bdd6277ab4345497a16fddde84bfebe5d16c5675
Erdian718 <erdian718@qq.com>
2025-09-25 15:02:36 +0800

    Implement CellCollection

--- Erdian718.Office.Spreadsheet/CellCollection.cs
+++ Erdian718.Office.Spreadsheet/CellCollection.cs
@@ -5,31 +5,51 @@ namespace Erdian718.Office.Spreadsheet;
 /// <summary>
 /// Represents a collection of cells in a row.
 /// </summary>
-public abstract class CellCollection : IEnumerable<Cell>
+public class CellCollection(IEnumerable<Cell> cells) : IEnumerable<Cell>
 {
+    private static readonly Cell _emptyCell = new EmptyCell();
+
+    private readonly Cell[] _cells = [.. cells];
+
     /// <summary>
     /// Gets a cell from the collection by column index.
     /// </summary>
     /// <param name="index">The zero-based index of the cell to retrieve.</param>
     /// <returns>The Cell at the specified index.</returns>
-    public abstract Cell this[int index] { get; }
+    public Cell this[int index] => index < _cells.Length ? _cells[index] : _emptyCell;
 
     /// <summary>
     /// Gets a cell from the collection by column reference.
     /// </summary>
     /// <param name="reference">The cell reference.</param>
     /// <returns>The Cell corresponding to the specified reference.</returns>
-    public virtual Cell this[ReadOnlySpan<char> reference] => this[SpreadsheetUtils.ColumnIndex(reference)];
+    public Cell this[ReadOnlySpan<char> reference] => this[SpreadsheetUtils.ColumnIndex(reference)];
+
+    /// <summary>
+    /// Gets the number of elements contained in the collection.
+    /// </summary>
+    public int Length => _cells.Length;
 
     /// <summary>
     /// Returns an enumerator that iterates through the collection of cells.
     /// </summary>
     /// <returns>An IEnumerator&lt;Cell&gt; that can be used to iterate through the collection.</returns>
-    public abstract IEnumerator<Cell> GetEnumerator();
+    public IEnumerator<Cell> GetEnumerator() => ((IEnumerable<Cell>)_cells).GetEnumerator();
 
     /// <summary>
     /// Explicit implementation of IEnumerable.GetEnumerator.
     /// </summary>
     /// <returns>An IEnumerator that can be used to iterate through the collection.</returns>
-    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    IEnumerator IEnumerable.GetEnumerator() => _cells.GetEnumerator();
+
+    /// <summary>
+    /// Represents an empty cell.
+    /// </summary>
+    internal class EmptyCell : Cell
+    {
+        /// <summary>
+        /// Gets the underlying raw value stored in the cell.
+        /// </summary>
+        public override object? Value => null;
+    }
 }
